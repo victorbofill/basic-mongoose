44c96d1d9a43ba0f914a9b364270a01a161d16f4
victorbofill <victorm.bofill@gmail.com>
2018-04-17 17:02:32 -0700

    basic server framework complete

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const app = require('./lib/app');
+const connect = require('./lib/connect');
+
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27019/friends';
+
+connect(MONGODB_URI);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+});
