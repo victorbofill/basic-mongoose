5b6b369b89fd1147edb33c1c8e2c46b651bd6891
victorbofill <victorm.bofill@gmail.com>
2018-04-18 10:09:59 -0700

    updated port to work with travis

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./lib/app');
 const connect = require('./lib/connect');
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27019/friends';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/friends';
 
 connect(MONGODB_URI);
 
